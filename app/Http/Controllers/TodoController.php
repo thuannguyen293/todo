5f93c7421781a304ad366c054f02ccb7309a2f64
thuannguyen293 <phuthuan.ng@gmail.com>
2022-10-06 22:14:04 +0700

    init

--- app/Http/Controllers/TodoController.php
+++ app/Http/Controllers/TodoController.php
@@ -12,7 +12,7 @@ class TodoController extends Controller
     //
     public function index()
     {
-        $todos = Todo::all();
+        $todos = Todo::orderBy('completed', 'desc')->get();
         return view('todos.index', compact('todos'));
     }
 
--- resources/views/todos/index.blade.php
+++ resources/views/todos/index.blade.php
@@ -8,8 +8,13 @@
 
 <ul class="my-5">
     @foreach ($todos as $todo)
-        <li>
-            <a href="{{ route('todos.edit', ['id' => $todo->id]) }}">{{ $todo->title }}</a>
+        <li class="flex justify-between p-2">
+            <p class="{{ $todo->completed ? 'line-through' : '' }}">{{ $todo->title }}</p>
+            <div>
+                <a href="{{ route('todos.edit', ['id' => $todo->id]) }}" class="cursor-pointer rounded text-orange-400"><span class="fas fa-edit px-2"></span></a>
+                <span class="fas fa-check px-2 text-green-400 {{ $todo->completed ? 'text-green-400' : 'text-gray-300' }}"></span>
+            </div>
+            
         </li>
     @endforeach
 </ul>
